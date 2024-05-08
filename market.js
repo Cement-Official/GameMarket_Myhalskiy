ca57f922eb309cdf281335da250eeccd1c704717
Cement-Official <nmihalskiy007@gmail.com>
2024-05-08 09:49:27 +0300

    g

--- market.js
+++ market.js
@@ -1,20 +1,38 @@
 
-console.log('Первірка підключеного файлу скриптів market.js')
+console.log('Підключено файл скриптів market.js')
 
 let itemsDiv = document.getElementById("items");
+let itemsArray = [
+    'Газонокосарка 43',
+    'Електричний тример 110',
+    'Електрична газонокосарка 32',
+    'Акумуляторний оприскувач 12 N',
+    'Газонокосарка 430',
+    'Електричний тример 130',
+    'Електрична газонокосарка 320',
+    'Акумуляторний оприскувач 12 E',
+]
 
-console.log(itemsDiv)
+// console.log(itemsDiv)
 
 if (itemsDiv) {
-    console.log(itemsDiv)
-    // console.log('Поле classList: ', itemsDiv.classList)
+    itemsArray.forEach
+    // console.log(itemsDiv)
     // console.log('Поле id: ', itemsDiv.id)
-    // console.log('Поле clientWidth: ', itemsDiv.clientWidth)
-    // console.log('Поле innerHTML: ', itemsDiv.innerHTML)
-    itemsDiv.innerHTML += '<div class = "item"></div>'
-    itemsDiv.innerHTML += '<div class = "item"></div>'
-    itemsDiv.innerHTML += '<div class = "item"></div>'
-    itemsDiv.innerHTML += '<div class = "item"></div>'
+    // itemsDiv.innerHTML += '<div class = "item"></div>'
+    // itemsDiv.innerHTML += '<div class = "item"></div>'
+    // itemsDiv.innerHTML += '<div class = "item"></div>'
+    // itemsDiv.innerHTML += '<div class = "item"></div>'
 } else {
     console.log('Блок товарів не знайдено')
 }
+
+// itemsArray.sort().forEach((item, index) => {
+//     console.log(index + '-й елемент: ', item)
+// })
+// itemsArray = itemsArray.sort()
+// console.log(itemsArray)
+
+// for (let i = 0; i < itemsArray.length; i++){
+//     console.log(i + '-й елемент: ', itemsArray[i])
+// }
